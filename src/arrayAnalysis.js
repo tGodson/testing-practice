6a73ff719c99c1b4658f81bb5141c1d45faf5c8f
kiptubei <kiptubei@gmail.com>
2020-11-05 15:48:25 +0300

    Removed webpack and unused dependencies and eslint expetion rules,removed unused variable object

--- src/arrayAnalysis.js
+++ src/arrayAnalysis.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-plusplus */
-/* eslint-disable no-return-assign */
 const analyze = (ar) => {
   let sum = 0;
-  let object;
-  for (let i = 0; i < ar.length; i++) {
+  for (let i = 0; i < ar.length; i += 1) {
     sum += ar[i];
   }
 
-  return object = {
+  return {
     average: sum / ar.length,
     min: Math.min.apply(null, ar),
     max: Math.max.apply(null, ar),
